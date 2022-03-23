e0a7c5d0d131cc09e3023db884281549c8a1b7c8
Firdaus909 <muhammadzamzami109@gmail.com>
2022-03-23 11:39:15 +0700

    add an album

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './dataAlbum/single-sample';
 
 const SPOTIFY_CLIENT_ID = process.env.REACT_APP_SPOTIFY;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1 className='title'>Song Playlist</h1>
+      <div className="album">
+        <div className="album-image">
+          <img src={data.album.images[0].url} />
+        </div>
+        <div className="album-info">
+          <h1>{data.name}</h1>
+          <h4>{data.artists[0].name} - {data.album.name}</h4>
+          <button>Select</button>
+        </div>
+      </div>
     </div>
   );
 }
