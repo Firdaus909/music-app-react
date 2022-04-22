00b145c94e70e98d04bebf6a2ff765c66cc883ab
Firdaus909 <muhammadzamzami109@gmail.com>
2022-04-22 14:12:48 +0700

    fix auth

--- src/helper/auth.ts
+++ src/helper/auth.ts
@@ -3,7 +3,7 @@ import { generateRandomKey } from './functions';
 const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 const REDIRECT_URI =
   process.env.REACT_APP_REDIRECT_URL ||
-  `${process.env.REACT_APP_VERCEL_URL}/redirect`;
+  `https://${process.env.REACT_APP_VERCEL_URL}/redirect`;
 const AUTHORIZE_URL = 'https://accounts.spotify.com/authorize';
 const SCOPE = 'playlist-modify-private playlist-read-private';
 const STATE = generateRandomKey(16);
